31659fb589a47b2c2b6e2b4e4c4255d11bf60380
Tim Boisvert <tboisvert@asheavenue.com>
2013-02-20 14:08:04 -0500

    periodic updte

--- app/controllers/nice_controller.rb
+++ app/controllers/nice_controller.rb
@@ -1,7 +1,50 @@
 class NiceController < ApplicationController
   
+  DEFAULT_FAMILY = "vanilla"
+  SPECIAL_FAMILY = "5.0"
+  
   def index
-    
+  end
+  
+  def vanilla
+    width = params[:width]
+    height = params[:height]
+    family = DEFAULT_FAMILY
+    handleimage(width, height, family)
+  end
+  
+  def vanillasquare
+    width = params[:square]
+    height = params[:square]
+    family = DEFAULT_FAMILY
+    handleimage(width, height, family)    
+  end
+  
+  def family
+    width = params[:width]
+    height = params[:height]
+    family = SPECIAL_FAMILY
+    handleimage(width, height, family)      
+  end
+  
+  def familysquare
+    width = params[:square]
+    height = params[:square]
+    family = SPECIAL_FAMILY
+    handleimage(width, height, family)    
+  end
+  
+  private 
+  
+  def handleimage(desiredWidth, desiredHeight, family)
+      
+      #determine if landscape or portrait is being requested
+      orientation = (desiredWidth.to_i >= desiredHeight.to_i) ? "landscape" : "portrait"
+      
+      image = Magick::Image.read(File.expand_path("../../pics/#{family}/#{orientation}/vert1.jpg", __FILE__)).first
+      image.resize_to_fill!(desiredWidth.to_i, desiredHeight.to_i)
+      image_as_blob = image.to_blob { self.quality=50 }
+      send_data image_as_blob, :type => "image/jpeg", :disposition => "inline"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Nicenicejpg::Application.routes.draw do
   root :to => 'nice#index'
-  match '/' => 'nice#index'
-  match '/5.0/:width/:height' => 'nice#fivepointoh'
-  match '/5.0/:widthandheight' => 'nice#fivepointoh'
-  match '/:width/:height' => 'nice#vanilla'
-  match '/:widthandheight' => 'nice#vanilla'
+  match "/" => 'nice#index'
+  match '/5.0/:width/:height' => 'nice#fivepointoh', :constraints => { :width => /[0-9]+/, :height => /[0-9]+/ }
+  match '/5.0/:square' => 'nice#fivepointohsquare', :constraints => { :square => /[0-9]+/ }
+  match '/:width/:height' => 'nice#vanilla', :constraints => { :width => /[0-9]+/, :height => /[0-9]+/ }
+  match '/:square' => 'nice#vanillasquare', :constraints => { :square => /[0-9]+/ }
+  match '/*anythingelse' => 'nice#index'
 end
